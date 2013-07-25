a1db2f90d4b63b0d952181dbf13b3917198cbc26
afrezet <afrezet@clever-age.com>
2013-07-25 23:03:28 +0200

    adding service loading class

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GeorgetteParty\BaseBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree builder.
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        // insert your builder here
+    }
+}
---
+++ DependencyInjection/KrovitchExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GeorgetteParty\BaseBundle\DependencyInjection\Configuration;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use GeorgetteParty\BaseBundle\DependencyInjection\Configuration\Configuration;
+
+class KrovitchExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        //$configuration = new Configuration();
+        //$config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+    public function getAlias()
+    {
+        return 'krovitch';
+    }
+}
