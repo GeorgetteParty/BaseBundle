7904b93b173f40e5d5aa671beec6525bef6fda03
afrezet <afrezet@clever-age.com>
2013-09-08 21:05:04 +0000

    correction on bundles service and configuration loading

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GeorgetteParty\BaseBundle\DependencyInjection\Configuration;
+namespace GeorgetteParty\BaseBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
--- DependencyInjection/GeorgettePartyBaseExtension.php
+++ DependencyInjection/GeorgettePartyBaseExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GeorgetteParty\BaseBundle\DependencyInjection\Configuration;
+namespace GeorgetteParty\BaseBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -20,7 +20,7 @@ class GeorgettePartyBaseExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
